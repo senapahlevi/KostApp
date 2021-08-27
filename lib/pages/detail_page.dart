06fef5d5892956dc57aad48be3953e79675058dc
senapahlevi <senapahlevi1@gmail.com>
2021-08-27 11:29:05 +0700

    Add title and star kuratakeso kost

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -8,41 +8,145 @@ class DetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Stack(
-        children: [
-          Image.asset(
-            'assets/pic/thumbnailkuratakeso.png',
-            height: 350,
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.cover,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: edge,
-              vertical: 30,
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/pic/thumbnailkuratakeso.png',
+              height: 328,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  //buat back button make pop
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Image.asset(
-                    'assets/pic/btn_back.png',
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: edge,
+                vertical: 30,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    //buat back button make pop
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset(
+                      'assets/pic/btn_back.png',
+                      width: 40,
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/pic/whislist.png',
                     width: 40,
                   ),
+                ],
+              ),
+            ),
+            ListView(
+              children: [
+                SizedBox(
+                  height: 308,
                 ),
-                Image.asset(
-                  'assets/pic/whislist.png',
-                  width: 40,
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                    color: whiteColor,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Kuretakeso Kost",
+                                  style: blackTextStyle.copyWith(
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Text.rich(
+                                  TextSpan(
+                                    text: '\$52',
+                                    style: purpleTextStyle.copyWith(
+                                      fontSize: 16,
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                        text: '/month',
+                                        style: greyTextStyle.copyWith(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/pic/Icon_star_active.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/pic/Icon_star_active.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/pic/Icon_star_active.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/pic/Icon_star_active.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/pic/Icon_star_active.png',
+                                  width: 20,
+                                  color: Color(0xffF989BA1),
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
-            ),
-          )
-        ],
-      )),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
